0a40029605b76d9c79285c030e9735b2160c34c9
TBRWEB - Desenvolvimento de Sites <vinibrunheroto12@gmail.com>
2023-08-18 10:08:27 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let bd = document.documentElement
+let btn = document.getElementById("switch");
+
+let img = document.querySelector("#profile img")
+
+
+btn.addEventListener("click", toggleMode)
+
+function toggleMode() {
+  bd.classList.toggle('light')
+
+  if (bd.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
